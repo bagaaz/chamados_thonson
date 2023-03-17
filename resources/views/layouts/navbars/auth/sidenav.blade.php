d42d1a11bb368fede4760047781af8a6150b34bb
Gabriel de Oliveira Silva <gabriel@bagaaz.vps-kinghost.net>
2023-03-17 16:55:51 -0300

    Apenas um commit de inclusão

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -1,5 +1,5 @@
 <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
-    id="sidenav-main" style="z-index: 1000;">
+    id="sidenav-main" style="z-index: 1001;">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
